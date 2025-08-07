5d7a4ad745f6da8f282f3c206093109788c57815
Homyakadze <gm142007@gmail.com>
2025-08-07 10:55:02 +0300

    Added binance client

---
+++ internal/infra/http/binance.go
@@ -0,0 +1,95 @@
+package http
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+var (
+	ErrUnexpected = errors.New("unexpected error")
+	ErrBadData    = errors.New("wrong symbol or currency")
+)
+
+type BinanceClient struct {
+	log     *slog.Logger
+	client  *http.Client
+	baseURL string
+}
+
+func NewBinanceClient(log *slog.Logger, timeout time.Duration) *BinanceClient {
+	client := &http.Client{
+		Timeout: timeout,
+	}
+
+	baseURL := "https://api.binance.com/api/v3"
+
+	return &BinanceClient{
+		log:     log,
+		client:  client,
+		baseURL: baseURL,
+	}
+}
+
+type Cryptocurrency struct {
+	Price float64 `json:"price"`
+}
+
+func (c *BinanceClient) GetPrice(symbol string, currency string) (float64, error) {
+	const op = "BinanceClient.GetPrice"
+	log := c.log.With(slog.String("op", op),
+		slog.String("symbol", symbol),
+		slog.String("currency", currency))
+
+	priceURL := c.baseURL + fmt.Sprintf("/ticker/price?symbol=%s%s", symbol, currency)
+	resp, err := c.client.Get(priceURL)
+	if err != nil {
+		log.Error(fmt.Sprintf("fail to get price! error: %s", err))
+		return 0, ErrUnexpected
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Error(fmt.Sprintf("bad status code! code: %s", resp.Status))
+		if resp.StatusCode == http.StatusBadRequest {
+			return 0, ErrBadData
+		}
+		return 0, ErrUnexpected
+	}
+
+	var data []byte
+	_, err = resp.Body.Read(data)
+	if err != nil {
+		log.Error(fmt.Sprintf("fail to read response body! error: %s", err))
+		return 0, ErrUnexpected
+	}
+
+	cryptocur := &Cryptocurrency{}
+	err = json.Unmarshal(data, cryptocur)
+	if err != nil {
+		log.Error(fmt.Sprintf("fail to unmarshal response body! error: %s", err))
+		return 0, ErrUnexpected
+	}
+
+	return cryptocur.Price, nil
+}
+
+func (c *BinanceClient) SymbolExists(symbol string) (bool, error) {
+	const op = "BinanceClient.SymbolExists"
+	log := c.log.With(slog.String("op", op),
+		slog.String("symbol", symbol))
+
+	_, err := c.GetPrice(symbol, "USDT")
+	if err != nil {
+		if errors.Is(err, ErrBadData) {
+			return false, nil
+		}
+		log.Error(fmt.Sprintf("fail to check existence! error: %s", err))
+		return false, err
+	}
+
+	return true, nil
+}
